09478c90d0c77d16953194ae4a1bf324b3464249
Victor Chelaru <VicChelaru@gmail.com>
2016-12-19 22:01:06 -0700

    Buggy  moves

--- BrakeNeck/BrakeNeck/Entities/PlayerBuggy.cs
+++ BrakeNeck/BrakeNeck/Entities/PlayerBuggy.cs
@@ -15,6 +15,7 @@ using FlatRedBall.Math.Splines;
 using BitmapFont = FlatRedBall.Graphics.BitmapFont;
 using Cursor = FlatRedBall.Gui.Cursor;
 using GuiManager = FlatRedBall.Gui.GuiManager;
+using Microsoft.Xna.Framework;
 
 #if FRB_XNA || SILVERLIGHT
 using Keys = Microsoft.Xna.Framework.Input.Keys;
@@ -28,6 +29,12 @@ namespace BrakeNeck.Entities
 {
 	public partial class PlayerBuggy
 	{
+
+        public IPressableInput GasInput { get; set; }
+        public I1DInput SteeringInput { get; set; }
+        public I2DInput AimingInput { get; set; }
+        public IPressableInput ShootingInput { get; set; }
+
         /// <summary>
         /// Initialization logic which is execute only one time for this Entity (unless the Entity is pooled).
         /// This method is called when the Entity is added to managers. Entities which are instantiated but not
@@ -35,17 +42,60 @@ namespace BrakeNeck.Entities
         /// </summary>
 		private void CustomInitialize()
 		{
-
+            CreateKeyboardInput();
 
 		}
 
-		private void CustomActivity()
+        private void CreateKeyboardInput()
+        {
+            GasInput = InputManager.Keyboard.GetKey(Keys.W);
+            SteeringInput = InputManager.Keyboard.Get1DInput(Keys.A, Keys.D);
+
+            AimingInput = InputManager.Keyboard.Get2DInput(Keys.J, Keys.L, Keys.I, Keys.K);
+            ShootingInput = InputManager.Keyboard.GetKey(Keys.LeftControl);
+        }
+
+        private void CustomActivity()
 		{
+            PerformMovementInput();
 
+            PerformShootingInput();
 
 		}
 
-		private void CustomDestroy()
+        private void PerformMovementInput()
+        {
+            float movementRatio = 0;
+            if(GasInput.IsDown)
+            {
+                movementRatio = 1;
+            }
+
+            this.Velocity = movementRatio * this.RotationMatrix.Right * MaxSpeed;
+
+            // eventually adjust this...
+
+            var radianVelocity = MathHelper.ToRadians(RotationSpeed);
+            this.RotationZVelocity = -this.SteeringInput.Value * radianVelocity * movementRatio;
+        }
+
+        private void PerformShootingInput()
+        {
+            if(this.ShootingInput.IsDown)
+            {
+                var angle = (float)Math.Atan2(AimingInput.Y, AimingInput.X);
+                this.ShootBullet(angle);
+            }
+        }
+
+        private void ShootBullet(float angle)
+        {
+            var bullet = Factories.PlayerBulletFactory.CreateNew();
+            bullet.RotationZ = angle;
+            bullet.Velocity = BulletSpeed * bullet.RotationMatrix.Right;
+        }
+
+        private void CustomDestroy()
 		{
 
 
