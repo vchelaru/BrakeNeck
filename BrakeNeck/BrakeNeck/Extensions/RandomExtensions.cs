9bd3a3b467abf6e4c649354ce365a36c651f2b29
Justin Johnson <justin.d.johnson@gmail.com>
2016-12-31 12:58:26 -0700

    Started implementing sandstorm effect.

---
+++ BrakeNeck/BrakeNeck/Extensions/RandomExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FlatRedBall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrakeNeck.Extensions
+{
+    public static class RandomExtensions
+    {
+        public static float FloatInRange(this Random rand, float min, float max)
+        {
+            float range = max - min;
+            float chosen = (float)(rand.NextDouble() * range);
+            return min + chosen;
+        }
+    }
+}
